db886f7725705994517e62a3eb6ff370bd733d76
Samuel-cyber-lol <ximenacastroc500@gmail.com>
2026-07-03 14:05:23 -0600

    cambios de codigo

--- sketch.js
+++ sketch.js
@@ -1,17 +1,32 @@
 let preguntas = [
-  "Mis ríos y acuíferos están un poco secos... ¿Podrías hacerme el favor de cerrar la llave mientras te cepillas los dientes o te enjabonas las manos? ¿SÍ o NO?",
+  "Mis ríos y acuíferos están un poco secos... ¿Podrías hacerme el favor de cerrar la llave mientras te cepillas los dientes o te enjabonas las manos?",
   "Oh...Bueno, podiamos intentarlo en otro momento",
-  "Para producir luz, muchas fábricas queman carbón. Si te pido el favor de apagar los focos que no uses para que trabajen menos... ¿SÍ o NO?"
+  "Para producir luz, muchas fábricas queman carbón. Si te pido el favor de apagar los focos que no uses para que trabajen menos..."
 ];
 
-let indiceActual = 0; // Lleva el control de qué pregunta estamos mostrando
+let respuestasPositivas = [
+  "¡Gracias! ¡Eres un héroe del agua!",
+  "Está bien, pero recuerda que el agua es un recurso valioso.",
+  "¡Genial! Cada pequeño esfuerzo cuenta para cuidar nuestro planeta."
+];
+
+let respuestasNegativas = [
+  "¡Qué pena! Pero aún puedes ayudar a cuidar el agua.",
+  "No pasa nada, cada pequeño cambio también cuenta.",
+  "Gracias por intentarlo, tu apoyo también importa."
+];
+
+let indiceActual = 0;
 let textoActual = "";
 let contadorLetras = 0;
 let velocidad = 0.5;
+let estado = "pregunta";
+let textoMostrar = "";
+
 
 // Variables para los botones
-let botonSi;
-let botonNo;
+let botonA;
+let botonB;
 
 let miImagen; // 1. Declarar la variable global
 
@@ -21,64 +36,78 @@ function preload() {
 } 
 
 function setup() {
-  createCanvas(1100, 1000); // Subimos un poco el alto para que quepan los botones abajo
+  createCanvas(1920, 1080); // Subimos un poco el alto para que quepan los botones abajo
   textSize(24);
   textFont('Courier New');
   textAlign(LEFT, TOP);
   
   // 2. CREAMOS LOS BOTONES
-  // Botón SÍ
-  botonSi = createButton('SÍ');
-  botonSi.position(300, 800); // Posición en la pantalla (X, Y)
-  botonSi.size(100, 40);      // Tamaño (Ancho, Alto)
-  botonSi.mousePressed(siguientePregunta); // Qué pasa cuando le dan clic
+  botonA = createButton('A');
+  botonA.position(600, 850);
+  botonA.size(100, 40);
+  botonA.mousePressed(responderPositivo);
   
-  // Botón NO
-  botonNo = createButton('NO');
-  botonNo.position(600, 800);
-  botonNo.size(100, 40);
-  botonNo.mousePressed(siguientePregunta);
+  botonB = createButton('B');
+  botonB.position(1200, 850);
+  botonB.size(100, 40);
+  botonB.mousePressed(responderNegativo);
 }
 
 function draw() {
   background(0);
   
  imageMode(CENTER);
-  image(miImagen, 500, 250,1200,700); 
+  image(miImagen, 1000,300,1100,700); 
   
   fill(255); 
   
-  // Obtenemos el texto de la pregunta actual de la lista
-  let textoCompleto = preguntas[indiceActual];
+  let textoCompleto = estado === "pregunta" ? preguntas[indiceActual] : textoMostrar;
   
   textoActual = textoCompleto.substring(0, floor(contadorLetras));
   
-  text(textoActual, 100, 650, 900, 120);
+  text(textoActual, 500, 700, 900, 120);
   
   if (contadorLetras < textoCompleto.length) {
     contadorLetras += velocidad;
-    
-    // Mientras la máquina escribe, escondemos los botones para que no hagan trampa
-    botonSi.hide();
-    botonNo.hide();
+    botonA.hide();
+    botonB.hide();
   } else {
-    // Cuando la máquina termina de escribir, mostramos los botones
-    botonSi.show();
-    botonNo.show();
+    if (estado === "pregunta") {
+      botonA.show();
+      botonB.show();
+      botonA.html('A');
+      botonB.html('B');
+      botonA.mousePressed(responderPositivo);
+      botonB.mousePressed(responderNegativo);
+    } else {
+      botonA.show();
+      botonB.show();
+      botonA.html('SIGUIENTE');
+      botonB.html('SIGUIENTE');
+      botonA.mousePressed(siguientePregunta);
+      botonB.mousePressed(siguientePregunta);
+    }
   }
 }
 
-// 3. FUNCIÓN PARA PASAR A LA SIGUIENTE PREGUNTA
-function siguientePregunta() {
-  // Avanzamos al siguiente elemento de la lista
-  indiceActual++;
-  
-  // Si llegamos al final de las preguntas, volvemos a empezar
-  if (indiceActual >= preguntas.length) {
-    indiceActual = 0; 
-  }
-  
-  // REINICIAMOS la máquina de escribir para el nuevo texto
+function responderPositivo() {
+  textoMostrar = respuestasPositivas[indiceActual];
+  estado = "respuesta";
   contadorLetras = 0;
   textoActual = "";
+}
+
+function responderNegativo() {
+  textoMostrar = respuestasNegativas[indiceActual];
+  estado = "respuesta";
+  contadorLetras = 0;
+  textoActual = "";
+}
+
+function siguientePregunta() {
+  indiceActual = (indiceActual + 1) % preguntas.length;
+  estado = "pregunta";
+  contadorLetras = 0;
+  textoActual = "";
+  textoMostrar = "";
 }
